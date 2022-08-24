a9916a121702213617194b0c61d85b6fbff0c72b
lomantech <87063237+lomantech@users.noreply.github.com>
2022-08-24 17:57:57 +0300

    Add decode methods

---
+++ morse_decode.rb
@@ -0,0 +1,25 @@
+def decode_char(char)
+  morse_code = {
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
+    '--..' => 'Z'
+  }
+  morse_code.each do |key, value|
+    return value if key == char
+  end
+end
+
+def decode_word(word)
+  res = ''
+  word.split
+    .each do |letter|
+    res += decode_char(letter)
+  end
+  res
+end
+
+puts decode_char('.-')
+puts decode_word('-- -.--')
