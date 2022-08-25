cf2ec59d5a134a1020e119e293294d6f16d3c00c
Zelalem1222 <zelalemfissha394@gmail.com>
2022-08-25 10:39:14 +0300

    Add some tests

--- my_list.rb
+++ my_list.rb
@@ -14,4 +14,5 @@ end
   my_list = MyList.new(1, 2, 3, 4)
   puts my_list.all? { |e| e > 5 }
   puts my_list.any? { |e| e < 5 }
+  puts my_list.any? {|e| e == 5}
   print my_list.filter { |e| e.even? }
