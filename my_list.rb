62ca51bc14e6d9ba4fede4d9add5d64158f922d0
Zelalem1222 <zelalemfissha394@gmail.com>
2022-08-25 12:19:31 +0300

    Make changes based on review comment

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,4 @@
-require './my_enumerable'
+require_relative './my_enumerable'
 
 class MyList
   include MyEnumerable
@@ -7,7 +7,22 @@ class MyList
     @list = list
   end
 
-  def each()
-    @list.length.times { |i| yield @list[i] }
+  def each
+    return to_enum(:each) unless block_given?
+
+    counter = 0
+    while counter < @list.length
+      yield(@list[counter])
+      counter += 1
+    end
+    @list
   end
 end
+
+my_list = MyList.new(1, 2, 3, 4)
+
+puts(my_list.all?) { |e| e > 5 }
+puts(my_list.any?) { |e| e < 5 }
+puts(my_list.any?) { |e| e == 5 }
+print my_list.filter(&:even?)
+print my_list.filter(&:odd?)
