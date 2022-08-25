dca257b9210ac51fb4518f7965959686c16e5d38
lomantech <87063237+lomantech@users.noreply.github.com>
2022-08-25 16:34:05 +0300

    Add mylist class file

---
+++ my_list.rb
@@ -0,0 +1,17 @@
+require './my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(*list)
+    @list = list
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+  my_list = MyList.new(1, 2, 3, 4)
+  puts my_list.all? { |e| e > 5 }
+  puts my_list.any? { |e| e < 5 }
+  print my_list.filter { |e| e.even? }
